b59cced9d48fdd84326557c65dfd7c17cc59a065
Pascale Gordon <pascale_gordon@hotmail.com>
2020-11-10 21:19:36 -0500

    Edited app.js

--- app.js
+++ app.js
@@ -2,19 +2,55 @@
 
 window.onload= function(){
     var s_button = document.getElementsByTagName("button")[0];
-    var h_list = document.getElementById("s_list");
+    
+    var site = document.getElementById("result");
+    var search_input = document.getElementById("search");
+    var xhttp;
 
     s_button.addEventListener('click', search);
 
     function search(e){
         var xhttp = new XMLHttpRequest();
         e.preventDefault();
+        var search_value = search_input.value;
         xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        alert(xhttp.responseText);
+        if (this.readyState == 4 && this.status == 200 && search_value.length==0) {
+            site.innerHTML= this.responseText;
     }   
   };
-  xhttp.open("GET", "superheroes.php", true);
+  xhttp.open("GET", "http://localhost:8080/superheroes.php", true);
   xhttp.send();
+    } 
+    
+
+   /* s_button.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var xhttp = new XMLHttpRequest();
+        var search_value = search_input.value;
+        xhttp.onreadystatechange = loadQuote;
+        xhttp.open('GET',"http://localhost:8080/superheroes.php?query="+search_value+"" );
+        xhttp.send();
+  });
+
+
+  function loadQuote() {
+    if (xhttp.readyState === XMLHttpRequest.DONE) {
+      if (xhttp.status === 200) {
+        var response = xhttp.responseText;
+        site.innerHTML = response;
+      }
     }
-}
+}  */
+
+
+
+    }
+
+
+
+    
+    
+
+    
+
